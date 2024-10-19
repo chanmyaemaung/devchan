a9e34b190339b8f000c8b99216d4583c68f05e5e
Chen Lay <maungmaungyoker.mmy@gmail.com>
2024-10-19 17:31:20 +0700

    feat(database): map user entity to database table

---
+++ src/common/embedded/index.ts
@@ -0,0 +1,1 @@
+export * from './registry-dates.embedded';
--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { RegistryDates } from '@/common/embedded';
 
 @Entity()
 export class User {
@@ -16,4 +17,7 @@ export class User {
 
   @Column()
   password: string;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
 }
